e8ffefedf30c421c1843fe398bc92a95cd738e70
Banti Patel <86671763+banti377@users.noreply.github.com>
2024-12-03 17:42:41 +0530

    Delete ExpenseTrackerApp directory

--- ExpenseTrackerApp/screens/RecentExpenses.js
+++
@@ -1,8 +0,0 @@
-import ExpensesOutput from '@/components/ExpensesOutput/ExpensesOutput';
-import { Text } from 'react-native';
-
-function RecentExpenses() {
-  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
-}
-
-export default RecentExpenses;
